869edacab9fe821fbed04ca5e33210d711d121d3
Zongyang Ma <zoma@microsoft.com>
2019-10-16 16:17:20 +0800

    add LG file

---
+++ src/TemplateProviderLG.ts
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { TemplateEngine } from 'botbuilder-lg';
+import { Template } from '@conversationlearner/models'
+
+class RenderedActionArgument {
+    parameter: string;
+    value: string | null;
+}
+
+function GetTemplate(): Template[] {
+
+    let templates: Template[] = []
+
+    templates.push({
+        name: "greeting",
+        variables: [],
+        body: "",
+        validationError: null
+    })
+
+    templates.push({
+        name: "askName",
+        variables: [],
+        body: "",
+        validationError: null
+    })
+
+    templates.push({
+        name: "askColor",
+        variables: [{ key: "userName", type: "string" }],
+        body: "",
+        validationError: null
+    })
+
+    templates.push({
+        name: "colorResponse",
+        variables: [{ key: "userName", type: "string" }, { key: "color", type: "string" }],
+        body: "",
+        validationError: null
+    })
+
+    return templates
+}
+
+/* function GetArgumentNames(formString: string) {
+    // Get set of unique entities
+    let mustaches = formString.match(/{{\s*[\w\.]+\s*}}/g)
+    if (mustaches) {
+        let entities = [...new Set(mustaches.map(x => x.match(/[\w\.]+/)![0]))]
+        return entities
+    }
+    return []
+} */
+
+function RemoveEmptyArguments(formString: string) {
+    return formString.replace(/{{\s*[\w\.]+\s*}}/g, '')
+}
+
+function RenderTemplate(lg_filename: string, templateArguments: RenderedActionArgument[]) {
+    let engine = new TemplateEngine().addFile(lg_filename);
+    let template = GetTemplate()
+    if (template === null) {
+        return null
+    }
+
+    let templateString = JSON.stringify(template)
+    //let argumentNames = GetArgumentNames(templateString)
+    let argumentNames = new Array()
+    for (var i = 0; i < template.length; i++) {
+        argumentNames[i] = template[i].name;
+    }
+    console.log(argumentNames)
+
+    for (let argumentName of argumentNames) {
+        let renderedActionArgument = templateArguments.find(a => a.parameter == argumentName)
+        if (renderedActionArgument) {
+            //console.log(renderedActionArgument);
+            renderedActionArgument.value = engine.evaluateTemplate(renderedActionArgument.parameter);
+            console.log(">>>>>");
+            console.log(renderedActionArgument.value);
+            templateString = templateString.replace(new RegExp(`{{${argumentName}}}`, 'g'), renderedActionArgument.value || '')
+        }
+    }
+    templateString = RemoveEmptyArguments(templateString)
+    return JSON.parse(templateString)
+}
+
+
+let lg_filename = `${__dirname}/tem.lg`;
+console.log(lg_filename)
+
+let templateArguments = new Array();
+var temparg = new RenderedActionArgument();
+temparg.parameter = "colorResponse";
+temparg.value = "";
+
+templateArguments[0] = temparg;
+
+let result = RenderTemplate(lg_filename, templateArguments);
